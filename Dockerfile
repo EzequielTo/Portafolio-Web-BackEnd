ddc83c42118fc20333f1b8dce2c5eae3ca6c7761
Ezequiel <122380414+EzequielTo@users.noreply.github.com>
2023-05-15 08:24:18 -0300

    subida para hostear

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM amazoncorretto:11-alpine-jdk
-MAINTAINER TE
-COPY target/te-0.0.1-SNAPSHOT.jar  Te-app.jar
-ENTRYPOINT ["java","-jar","/Te-app.jar"]
+MAINTAINER Ezequiel Tokashiki  
+COPY target/te-0.0.1-SNAPSHOT.jar TE-app.jar
+ENTRYPOINT ["java","-jar","/TE-app.jar"]
--- src/main/java/com/ejemplo/SpringBoot/Security/Controller/AuthController.java
+++ src/main/java/com/ejemplo/SpringBoot/Security/Controller/AuthController.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://backend-alizunega.koyeb.app")
+@CrossOrigin(origins = {"https://localhost:8080","https://localhostt:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
